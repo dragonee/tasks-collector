9c4997b1dedaf4e39f5f4c263a2b26b5cc36a90f
Michał Moroz <michalmoroz@gmail.com>
2021-12-26 23:34:22 +0100

    Add close action to admin

--- tasks/apps/tree/admin.py
+++ tasks/apps/tree/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from .models import Board, Thread, Plan, Reflection, Observation, ObservationType
 
+from datetime import datetime
+
 # Register your models here.
 
 class ObservationTypeAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
+@admin.action(description='Close selected observations')
+def close_observations(modeladmin, request, queryset):
+    queryset.update(date_closed=datetime.today())
+
 class ObservationAdmin(admin.ModelAdmin):
     list_filter = ('date_closed',)
+
+    list_display = ('__str__', 'date_closed', 'thread', 'type')
+
+    actions = [close_observations]
 
 admin.site.register(Board)
 admin.site.register(Thread)
