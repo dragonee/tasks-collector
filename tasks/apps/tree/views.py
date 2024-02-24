2822d27d4bda4af907c8e69c5b36cb17f3590abe
Michał Moroz <michalmoroz@gmail.com>
2024-02-25 00:55:27 +0100

    Moved commit into separate file operating on board state only

--- tasks/apps/tree/views.py
+++ tasks/apps/tree/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from .serializers import BoardSerializer, BoardSummary, ThreadSerializer, PlanSerializer, ReflectionSerializer, ObservationSerializer
 from .models import Board, Thread, Plan, Reflection, Observation
 from .forms import PlanForm, ReflectionForm
+from .commit import merge, calculate_changes_per_board, pprint
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -17,16 +18,8 @@ from django.shortcuts import get_object_or_404
 
 from django.views.generic.list import ListView
 
-from functools import reduce, partial
-from collections import OrderedDict
-
-from itertools import groupby
-
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
-
-
-from copy import deepcopy
 
 import datetime
 
@@ -76,60 +69,6 @@ class ThreadViewSet(viewsets.ModelViewSet):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
 
-def transition_markers_in_tree_item(markers):
-    new_markers = {
-        "weeksInList": 0 if markers.get('madeProgress', False) else markers['weeksInList'] + 1,
-        "important": markers['important'],
-        "finalizing": markers['finalizing'],
-        "canBeDoneOutsideOfWork": markers['canBeDoneOutsideOfWork'],
-        "canBePostponed": markers['canBePostponed'],
-        "postponedFor": max(0, markers['postponedFor'] - 1),
-        "madeProgress": False,
-    }
-
-    value = markers.get('transition')
-
-    if value:
-        new_markers['transition'] = value
-
-    return new_markers
-
-def transition_data_in_tree_item(item):
-    return {
-        "text": item['text'],
-        "children": transition_data_between_boards(item['children']),
-
-        "data": {
-            "text": item['data']['text'],
-            "state": item['data']['state'],
-            "meaningfulMarkers": transition_markers_in_tree_item(item['data']['meaningfulMarkers']),
-        },
-
-        'state': {
-            'visible': True if item['data']['meaningfulMarkers']['postponedFor'] == 1 else item['state']['visible']
-        }
-    }
-
-def filter_out_checked_items(x):
-    """Return True if should be filtered out"""
-
-    if x.get('data', {}).get('meaningfulMarkers', {}).get('canBePostponed', False):
-        return False
-
-    return x.get('state', {}).get('checked', False)
-
-
-def transition_data_between_boards(state):
-    items = filter(lambda x: not filter_out_checked_items(x), state)
-
-    return list(map(transition_data_in_tree_item, items))
-
-def recursively(collection, f, c, key='children'):
-    return c([c((recursively(item[key], f, c), f(item))) for item in collection])
-
-def unionize_sets(items):
-    return reduce(lambda x, y: x.union(y if isinstance(y, set) else set([y])), items, set())
-
 def make_board(thread_name):
     thread=Thread.objects.get(name=thread_name)
 
@@ -138,86 +77,30 @@ def make_board(thread_name):
     except IndexError:
         return Board(thread=thread)
 
-def cut_leaves(board_state, thread_name, implied=False):
-    new_board_state = []
-
-    for item in board_state:
-        # None or string
-        item_thread_name = item['data']['meaningfulMarkers'].get('transition')
-
-        new_implied = item_thread_name or implied
-
-        children = cut_leaves(item['children'], thread_name, implied=new_implied)
-
-        if children or (item_thread_name == thread_name and implied == False) or implied == thread_name:
-            try:
-                del item['data']['meaningfulMarkers']['transition']
-            except KeyError:
-                pass
-
-            new_board_state.append(item)
-
-        item['children'] = children
-
-    return new_board_state
-
-def merge(a, b):
-    names_a = OrderedDict((item['data']['text'], index) for index,item in enumerate(a))
-
-    names_b = OrderedDict((item['data']['text'], index) for index,item in enumerate(b))
-
-    print(names_a, names_b)
-
-    common = set(names_a.keys()).intersection(names_b.keys())
-
-    in_b = set(names_b.keys()).difference(names_a.keys())
-
-    for key in common:
-        item_a = a[names_a[key]]
-        item_b = b[names_b[key]]
-
-        item_a['children'] = merge(item_a['children'], item_b['children'])
-
-    return a + [b[names_b[key]] for key in in_b]
-
-def pprint(board_state, level=0):
-    for item in board_state:
-        print("  " * level, item['data']['text'], flush=True)
-        pprint(item['children'], level + 1)
 
 @api_view(['POST'])
 def commit_board(request, id=None):
     board = Board.objects.get(pk=id)
 
+    changeset = calculate_changes_per_board(board.state)
+
+    # for name, changes in changeset.items():
+    #     print("Board ({})".format(name))
+    #     pprint(changes)
+    #     print('------------------', flush=True)
+
     new_board = Board()
     new_board.thread = board.thread
 
-    new_board.state = transition_data_between_boards(board.state)
-    # recursively find all boards referenced by "transition fields"
-    thread_names = recursively(
-        new_board.state,
-        lambda item: item['data']['meaningfulMarkers'].get('transition'),
-        unionize_sets
-    )
+    if None in changeset:
+        new_board.state = changeset[None]
+        del changeset[None]
 
-    other_boards = list(map(make_board, thread_names.difference({None})))
+    other_boards = list(map(make_board, changeset.keys()))
 
-    # dfs with leaf-cutting
     for other_board in other_boards:
-        other_board.state = merge(other_board.state, cut_leaves(deepcopy(new_board.state), other_board.thread.name))
-
-
-    #print(thread_names.difference({None}))
-    new_board.state = cut_leaves(new_board.state, None)
-
-    # merge boards
-    for other_board in other_boards:
-        #print("Board ({})".format(other_board.thread.name))
-        #pprint(other_board.state)
-        #print('------------------', flush=True)
+        other_board.state = merge(other_board.state, changeset[other_board.thread.name])    
         other_board.save()
-
-    #pprint(new_board.state)
 
     board.date_closed = timezone.now()
     board.save()
