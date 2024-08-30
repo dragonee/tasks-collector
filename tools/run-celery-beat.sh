8497ee3faeef0c61d465f7c37e8133abe348ca48
Michał Moroz <michalmoroz@gmail.com>
2024-08-30 22:04:43 +0200

    feat: Use one container for celery worker/beat instead of two

--- tools/run-celery-beat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-REQUIREMENTS=/app/requirements/local.txt
-
-pip3 install -r $REQUIREMENTS
-/app/manage.py runcelery beat -l info
--- tools/run-celery.sh
+++ tools/run-celery.sh
@@ -5,4 +5,4 @@ set -e
 REQUIREMENTS=/app/requirements/local.txt
 
 pip3 install -r $REQUIREMENTS
-/app/manage.py runcelery worker -l info
+/app/manage.py runcelery worker -l info --beat
