939a44df8d45d18bd5d19fd016d2c99319e32898
Michał Moroz <michalmoroz@gmail.com>
2024-09-23 23:40:24 +0200

    fix: Remove add_published

--- tasks/apps/tree/templatetags/model_presenters.py
+++ tasks/apps/tree/templatetags/model_presenters.py
@@ -9,17 +9,6 @@ register = template.Library()
 
 
 @register.filter
-def add_published(object):
-    linebreaks_str = linebreaks(object.comment)
-
-    time_str = '<p data-time="{}">'.format(
-        object.published.strftime('%H:%M')
-    )
-
-    return mark_safe(linebreaks_str.replace('<p>', time_str, 1))
-
-
-@register.filter
 @stringfilter
 def first_line(text):
     splitted = text.split('\n')
