a6f5be16badbd09cb4f73c4409ee69651663cd52
Michał Moroz <michalmoroz@gmail.com>
2025-01-03 12:41:27 +0100

    feat(breakthrough): show missing months in timeline

--- tasks/apps/tree/templatetags/model_presenters.py
+++ tasks/apps/tree/templatetags/model_presenters.py
@@ -6,6 +6,8 @@ from django.template.defaultfilters import stringfilter
 
 from ..forms import OnlyTextSingleHabitTrackedForm
 
+from django.template.defaultfilters import date as date_filter
+from datetime import date
 
 register = template.Library()
 
@@ -36,3 +38,26 @@ def get_habit_pattern(habit):
 @register.filter
 def habit_without_name(tracked):
     return re.sub(get_habit_pattern(tracked.habit), '', tracked.note)
+
+@register.filter
+def missing_months(regrouped_habits):
+    year = regrouped_habits[0][1][0].published.year
+
+    months = [date_filter(date(year, month, 1), 'F Y') for month in range(1, 13)]
+
+    def pair_iterators(full_iter, partial_iter):
+        full_iter = iter(full_iter)
+        partial_iter = iter(partial_iter)
+        
+        full_item = next(full_iter, None)
+        partial_item = next(partial_iter, None)
+        
+        while full_item is not None:
+            if partial_item is not None and full_item == partial_item[0]:
+                yield (full_item, partial_item[1])
+                partial_item = next(partial_iter, None)
+            else:
+                yield (full_item, [])
+            full_item = next(full_iter, None)
+
+    return pair_iterators(months, regrouped_habits)
